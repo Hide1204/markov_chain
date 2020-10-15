3a9f8f76b69ae4568a48571f83e129853751a652
Hide1204 <hirahide1204@gmail.com>
2020-10-15 13:37:19 +0900

    形態素解析を行って出力できた

--- markov.py
+++ markov.py
@@ -1,1 +1,23 @@
+import re
+import MeCab
+
 import ignore_words
+#from PrepareChain import PrepareChain
+import round_up_tweets
+
+def wakati(text):
+    t = MeCab.Tagger("-Owakati")
+    return t.parse(text)
+
+def main():
+    pass
+
+def test():
+    tweet_data=round_up_tweets.test()
+    for text in tweet_data:
+        print(text)
+        print(wakati(text))
+    
+if __name__ == "__main__":
+    #main()
+    test()
